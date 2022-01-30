67f451fd0755b4effdc40937c600c0b13ab0e547
h4nyu <yao.ntno@gmail.com>
2022-01-30 11:46:07 +0900

    add ci

--- dockerfiles/onnx-tf/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-sudo usermod -u ${UID} developer
-sudo groupmod -g ${GID} developer
-
-python /scripts/onnx_tf_frontend.py "$@"
--- dockerfiles/timm-onnx/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-sudo usermod -u ${UID} developer
-sudo groupmod -g ${GID} developer
-
-python /scripts/timm_onnx.py "$@"
