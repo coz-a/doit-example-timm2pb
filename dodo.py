a3c001170e3ec3c02d65fc78e8d90bc657dff6d1
h4nyu <yao.ntno@gmail.com>
2022-01-30 09:40:06 +0900

    ?

--- dodo.py
+++ dodo.py
@@ -7,20 +7,13 @@ output_root = get_var("output-root", "compiled")
 output_dir = Path(output_root) / model_name
 output_dir.mkdir(parents=True, exist_ok=True)
 
-DOCKER_COMMANDLINE_BASE = (
-    "docker run -it --rm "
-    "-e UID=$(id -u $USER) -e GID=$(id -g $USER) "
-    "-v $(pwd):/workspace "
-)
-
-
 def task_timm_onnx():
     tgt_model_file = str(output_dir / "model.onnx")
     tgt_metadata_file = str(output_dir / "metadata.json")
     return {
         "targets": [tgt_model_file, tgt_metadata_file],
         "actions": [
-            DOCKER_COMMANDLINE_BASE + f"timm-onnx --model-name {model_name} --model-output {tgt_model_file} --metadata-output {tgt_metadata_file}"
+            f"docker-compose run --rm timm-onnx --model-name {model_name} --model-output {tgt_model_file} --metadata-output {tgt_metadata_file}"
         ],
         "uptodate": [True]
     }
@@ -33,6 +26,6 @@ def task_onnx_tf():
         "file_dep": [dep_file],
         "targets": [tgt_file],
         "actions": [
-            DOCKER_COMMANDLINE_BASE + f"onnx-tf --input {dep_file} --output {tgt_file}"
+            f"docker-compose run --rm onnx-tf --input {dep_file} --output {tgt_file}"
         ]
     }
